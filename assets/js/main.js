bb6f65ea2928ae1340300834228f499329a333dc
kodehat <dev@codehat.de>
2024-02-18 13:09:10 +0100

    Add JavaScript with bundler, Add dark/light mode with toggle

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+import Alpine from "alpinejs";
+import persist from '@alpinejs/persist'
+
+window.Alpine = Alpine;
+
+Alpine.plugin(persist);
+Alpine.start();
+
+console.log("JavaScript is running!");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./components/*.templ"],
   theme: {
     extend: {},
